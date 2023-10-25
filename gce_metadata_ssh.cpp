a9cd59ce0767857a39dbe94ab4435d4803db301f
Alexander von Gluck IV <kallisti5@unixzen.com>
2023-10-24 19:59:40 -0500

    style: Fix the weird spacing; non-functional change

--- gce_metadata_ssh.cpp
+++ gce_metadata_ssh.cpp
@@ -1,4 +1,5 @@
-/* Copyright 2022, Alexander von Gluck IV <kallisti5@unixzen.com>
+/*
+ * Copyright 2022, Alexander von Gluck IV <kallisti5@unixzen.com>
  * Released under the terms of the MIT license.
  */
 
@@ -16,57 +17,57 @@
 size_t
 curl_write(void* ptr, size_t size, size_t members, std::string* s)
 {
-		s->append(static_cast<char*>(ptr), size * members);
-		return size * members;
+	s->append(static_cast<char*>(ptr), size * members);
+	return size * members;
 }
 
 
 char*
 get_metadata_keys(char* username)
 {
-		CURL* curl;
-		CURLcode res;
-		struct curl_slist *headers = NULL;
+	CURL* curl;
+	CURLcode res;
+	struct curl_slist *headers = NULL;
 
-		std::string result;
-		char url[128];
-		snprintf(url, sizeof(url) - 1, "%s%s", GCE_METADATA_BASE, "instance/attributes/ssh-keys");
+	std::string result;
+	char url[128];
+	snprintf(url, sizeof(url) - 1, "%s%s", GCE_METADATA_BASE, "instance/attributes/ssh-keys");
 
-		curl = curl_easy_init();
-		if (curl) {
-				headers = curl_slist_append(headers, "Metadata-Flavor: Google");
+	curl = curl_easy_init();
+	if (curl) {
+		headers = curl_slist_append(headers, "Metadata-Flavor: Google");
 
-				curl_easy_setopt(curl, CURLOPT_URL, url);
-				curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write);
-				curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
-				curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-				res = curl_easy_perform(curl);
-				curl_easy_cleanup(curl);
-				curl_slist_free_all(headers);
+		curl_easy_setopt(curl, CURLOPT_URL, url);
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+		curl_slist_free_all(headers);
 
-				// XXX: There's probably a better way to do this...
-				std::istringstream stream(result);
-				std::string line;
-				while (std::getline(stream, line)) {
-						std::string user = line.substr(0, line.find(':'));
-						std::string key = line.substr(line.find(':') + 1);
-						if (user == username)
-							std::cout << key << "\n";
-				}
-		}
-		return NULL;
+		// XXX: There's probably a better way to do this...
+		std::istringstream stream(result);
+		std::string line;
+		while (std::getline(stream, line)) {
+			std::string user = line.substr(0, line.find(':'));
+			std::string key = line.substr(line.find(':') + 1);
+			if (user == username)
+				std::cout << key << "\n";
+			}
+	}
+	return NULL;
 }
 
 
 int
 main(int argc, char** argv)
 {
-		if (argc != 2) {
-				printf("usage: %s <username>\n", argv[0]);
-				return 1;
-		}
+	if (argc != 2) {
+		printf("usage: %s <username>\n", argv[0]);
+		return 1;
+	}
 
-		get_metadata_keys(argv[1]);
-		return 0;
+	get_metadata_keys(argv[1]);
+	return 0;
 }
 
